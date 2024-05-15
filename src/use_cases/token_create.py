e60e93786e13124b0880c54ada76bc59ee6a2499
Igor de Freitas Cruz <igor.freitas.cruz@icloud.com>
2024-05-15 11:30:12 -0300

    feat: criar roles e permissões de usuários

--- src/use_cases/token_create.py
+++ src/use_cases/token_create.py
@@ -1,8 +1,6 @@
-from src.error.types.http_bad_request import BadRequestError
 from src.presentation.http_types.http_request import HttpRequest
-from src.repository.model_objects import Usuario
 from src.repository.repo_usuario import UsuarioRepo
-from src.security import create_access_token, verify_password
+from src.security.security import login_user
 
 
 class TokenCreateUseCase:
@@ -10,32 +8,8 @@ class TokenCreateUseCase:
         self.usuario_repository = usuario_repository
 
     def execute(self, http_request: HttpRequest):
-        token_data: dict = http_request.json
+        login_data: dict = http_request.json
+        email: str = login_data.get('email')
+        password: str = login_data.get('password')
 
-        usuario: dict = self._check_usuario_exists(token_data.get('email'))
-
-        self._verify_password(
-            token_data.get('password'), usuario.get('hash_password')
-        )
-
-        del usuario['hash_password']
-        token = self._create_access_token(usuario)
-
-        return {'access_token': token, 'token_type': 'bearer'}
-
-    def _check_usuario_exists(self, usuario_email: str):
-        usuario = self.usuario_repository.get_usuario_by_email(usuario_email)
-
-        if usuario is None:
-            raise BadRequestError('Email ou senha incorretos')
-
-        return usuario
-
-    def _verify_password(self, usuario_password: str, hashed_password: str):
-        valid_password = verify_password(usuario_password, hashed_password)
-
-        if not valid_password:
-            raise BadRequestError('Email ou senha incorretos')
-
-    def _create_access_token(self, usuario: dict):
-        return create_access_token(data={'sub': usuario})
+        return login_user(email, password)
